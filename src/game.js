a48aa1a88dbf8d7ee174e2eeb5f3b9711acd5830
William Leung <w.leung.93@gmail.com>
2021-01-05 16:11:26 -0800

    using canvas

--- src/game.js
+++ src/game.js
@@ -1,126 +1,297 @@
-//dimension of the game board
-const DIM_X = 800;
-const DIM_Y = 650;
+//!NON - CANVAS WAY
+// //dimension of the game board
+// const DIM_X = 800;
+// const DIM_Y = 650;
 
-//ships position when game is initialized
-const SHIP_POS = {
-    x: DIM_X / 2,
-    y: (DIM_Y / 2) + 100
-}
+// //ships position when game is initialized
+// const SHIP_POS = {
+//     x: DIM_X / 2,
+//     y: (DIM_Y / 2) + 100
+// }
+
+// //when game is initialize, it will create a player and give it a position
+// function createPlayer(_game){
+//     const _ship = document.createElement("IMG");
+//     _ship.src = '../stylesheets/img/player_green.png';
+//     _ship.className = 'ship';
+//     _ship.style.height = 'auto';
+//     _ship.style.width = '40px';
+
+//     //adding the ship element INTO the game element tag as the last child
+//     _game.appendChild(_ship);
+
+//     //starts the game with the ship in the SHIP_POS
+//     _ship.style.transform = `translate(${SHIP_POS.x}px, ${SHIP_POS.y}px)`
+
+// }
 
 
-//keypress is deprecated
-window.addEventListener('keydown', onKeyPress);
+// //keypress is deprecated
+// window.addEventListener('keydown', onKeyPress);
 // window.addEventListener('keyup', onKeyUp);
+// window.requestAnimationFrame(update); //lets the browser know to annimate something
 
-const MOVE_DIR = {
-    left: 'ArrowLeft',
-    right: 'ArrowRight',
-    up: 'ArrowUp',
-    down: 'ArrowDown',
+// const MOVE_DIR = {
+//     left: 'ArrowLeft',
+//     right: 'ArrowRight',
+//     up: 'ArrowUp',
+//     down: 'ArrowDown',
+//     space: " "
+// }
 
-    // left: false,
-    // right: false,
-    // up: false,
-    // down: false,
-    space: " "
+// const KEY_PRESS = {
+//     left: false,
+//     right: false,
+//     up: false,
+//     down: false
+// }
+
+// function onKeyPress(e){
+//     e.preventDefault();
+//     let key = e.key;
+//     const _ship = document.querySelector('.ship')
+
+//     if(key === MOVE_DIR.left){
+//         // SHIP_POS.x -= 23;
+//         // console.log(SHIP_POS.x)
+//         // _ship.style.transform = `translate(${SHIP_POS.x}px, ${SHIP_POS.y}px)`    
+//         KEY_PRESS.left = true;
+//     }else if( key === MOVE_DIR.right ){
+//         KEY_PRESS.right = true;
+//     }else if ( key === MOVE_DIR.up){
+//         KEY_PRESS.up = true;
+//     }else if ( key === MOVE_DIR.down ){
+//         KEY_PRESS.down = true;
+//     }else if ( key === MOVE_DIR.space ){
+//         console.log('pew');
+//     }
+// }
+
+// function onKeyUp(e){
+//     let key = e.key;
+//     const _ship = document.querySelector('.ship')
+
+//     if(key === MOVE_DIR.left){   
+//         KEY_PRESS.left = false;
+//     }else if( key === MOVE_DIR.right ){
+//         KEY_PRESS.right = false;
+//     }else if ( key === MOVE_DIR.up){
+//         KEY_PRESS.up = false;
+//     }else if ( key === MOVE_DIR.down ){
+//         KEY_PRESS.down = false;
+//     }else if ( key === MOVE_DIR.space ){
+//         console.log('pew');
+//     }
+// }
+
+
+// function changeShipPos(){
+//     if (KEY_PRESS.left){
+//         SHIP_POS.x > 0 ? SHIP_POS.x -= 4 : 0
+//         // SHIP_POS.x > 0 ? SHIP_POS.x -= 4 : 0
+//         // SHIP_POS.x -= 4;
+//         // console.log('ship',SHIP_POS.x);
+//         // console.log(DIM_X - 40);
+//     }
+//     if (KEY_PRESS.right){
+//         SHIP_POS.x < DIM_X - 40 ? SHIP_POS.x += 4 : DIM_X - 40
+//     }
+//     if (KEY_PRESS.up){
+//         SHIP_POS.y > 0 ? SHIP_POS.y -= 4 : 0
+//     }
+//     if (KEY_PRESS.down){
+//         SHIP_POS.y < DIM_Y - 40 ? SHIP_POS.y += 4 : DIM_Y - 40
+//     }   
+
+//     let horiBorder = borderRestriction(SHIP_POS.x, 0, (DIM_X - 40))
+//     let veriBorder = borderRestriction(SHIP_POS.y, 0, (DIM_Y - 40))
+
+//     executeShipPos(horiBorder, veriBorder); 
+// }
+
+// function executeShipPos(x, y){
+//     const _ship = document.querySelector('.ship');
+//     _ship.style.transform = `translate(${x}px, ${y}px)`
+// }
+
+
+// function borderRestriction(pos, min, max){
+//     if( pos < min ){
+//         return min;
+//     }else if ( pos > max){
+//         return max;
+//     }else {
+//         return pos;
+//     }
+// }
+
+
+// function update(){
+//     // const deltaTime = (Date.now() - SHIP_POS.lastTime) / 1000;
+
+//     changeShipPos();
+//     window.requestAnimationFrame(update)
+// }
+
+
+
+// //initializes the game
+// function initialize(){
+//     let _game = document.getElementById("game")
+
+//     createPlayer(_game);
+//     console.log('Game initialized')
+//     console.log('Game test')
+// }
+
+// initialize();  
+
+//!~~~~~~~~~~~~~~~~~~~CANVAS TEST~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const SHIP = new Image();
+    SHIP.src = '../stylesheets/img/player_green.png';
+// SHIP.className = 'ship';
+// SHIP.style.height = 'auto';
+// SHIP.style.width = '40px';
+const BACKGROUND = new Image();
+    BACKGROUND.src = '../stylesheets/img/bg.png';
+
+
+function Drawable(){
+    this.initialize = function(x, y){ //sets the x and y position when game starts
+        this.x = x;
+        this.y = y;
+    }
+    this.speed = 0; // pixels fps 
+    this.canvasWidth = 0;
+    this.canvasHeight = 0;
+    this.draw = function(){}; //indicator that this function isnt to create objects
+
 }
 
-const DIAG_DIR = {
-    left: false,
-    right: false,
-    up: false,
-    down: false
+function Background(){
+    this.speed = 1; 
+
+    this.draw = function(){
+        this.y += this.speed; //1 pixel per frame
+        this.context.drawImage(BACKGROUND, this.x, this.y);
+        this.context.drawImage(BACKGROUND, this.x, this.y - this.canvasHeight); 
+        //redraw the same image twice so it appears to be infinite scrolling
+
+        if (this.y >= this.canvasHeight){
+            this.y = 0;
+        }
+        //if the image leaves the screen, it will restart
+    }   
 }
+Background.prototype = new Drawable();
+//telling Background to inherit everything from Drawable;
 
 
-//when game is initialize, it will create a player and give it a position
-function createPlayer(_game){
-    const _ship = document.createElement("IMG");
-    _ship.src = '../stylesheets/img/player_green.png';
-    _ship.className = 'ship';
-    _ship.style.height = 'auto';
-    _ship.style.width = '40px';
-    // _ship.alt = 'test';
-    console.log(_ship)
 
-    //adding the ship element INTO the game element tag as the last child
-    _game.appendChild(_ship);
 
-    //starts the game with the ship in the SHIP_POS
-    _ship.style.transform = `translate(${SHIP_POS.x}px, ${SHIP_POS.y}px)`
+function Game(){
+    this.initialize = function(){
+        this.bgCanvas = document.getElementById('background');
+        this.bgContext = this.bgCanvas.getContext('2d');
 
-}
+        Background.prototype.context = this.bgContext;
+        Background.prototype.canvasWidth = this.bgCanvas.width;
+        Background.prototype.canvasHeight = this.bgCanvas.height;
 
-function onKeyPress(e){
-    // let hi = document.addEventListener('keydown', fn);
-    // console.log(e.key)
-    let key = e.key;
-    const _ship = document.querySelector('.ship')
+        this.background = new Background();
+        this.background.initialize(0, 0);
 
-    if(key === MOVE_DIR.left){
-        SHIP_POS.x -= 23;
-        console.log(SHIP_POS.x)
-        _ship.style.transform = `translate(${SHIP_POS.x}px, ${SHIP_POS.y}px)`    
-    }else if( key === MOVE_DIR.right ){
-        SHIP_POS.x += 23;
-        console.log(SHIP_POS.x)
-        _ship.style.transform = `translate(${SHIP_POS.x}px, ${SHIP_POS.y}px)`
-    }else if ( key === MOVE_DIR.up && DIAG_DIR.left === false && DIAG_DIR.right === false){
-        SHIP_POS.y -= 23;
-        console.log(SHIP_POS.y)
-        _ship.style.transform = `translate(${SHIP_POS.x}px, ${SHIP_POS.y}px)`
-    }else if ( key === MOVE_DIR.down ){
-        SHIP_POS.y += 23;
-        console.log(SHIP_POS.y)
-        _ship.style.transform = `translate(${SHIP_POS.x}px, ${SHIP_POS.y}px)`
-    }else if ( key === MOVE_DIR.space ){
-        console.log('pew');
+    }
+
+    this.start = function(){
+        animate();
     }
 }
 
-//!diagonal test
-// function onKeyUp(e){
-//     let key = e.key;
 
-//     if(key === MOVE_DIR.left){
-//         DIAG_DIR.left = false;
-//         console.log('released clicked', DIAG_DIR.left)
-        
-//     }else if( key === MOVE_DIR.right ){
-//         console.log('up',SHIP_POS.x)
-        
-//         //test
-//         DIAG_DIR.right = false;
-
-
-//     }else if ( key === MOVE_DIR.up ){
-//         DIAG_DIR.up = false;
-//         console.log('released clicked', DIAG_DIR.up)
-        
-
-
-//     }else if ( key === MOVE_DIR.down ){
-//         console.log('up',SHIP_POS.y)
-        
-//         //test
-//         DIAG_DIR.down = false;
-//     }
-// }
-//!diagonal test
-
-//initializes the game
-function initialize(){
-    let _game = document.getElementById("game")
-
-    createPlayer(_game);
-    console.log('Game initialized')
-    console.log('Game test')
+function animate(){
+    window.requestAnimationFrame(animate);
+    game.background.draw();
 }
 
-initialize();  
+
+//  //lets the browser know to annimate something
+// window.requestAnimationFrame = (function(){
+//     return window.requestAnimationFrame
+// })
+
+
+
+// Initialize Game
+let game = new Game();
+
+function initialize(){
+    game.initialize();
+    game.start();
+}
 
 
 
 
-//!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TEST~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//!~~~~~~~~~~~~~~~~~~~CANVAS TEST  -  ORIGINAL~~~~~~~~~~~~~~~~~~~~~~~~~
+// const canvas = document.getElementById('canvas1');
+// const ctx = canvas.getContext('2d');
+// canvas.width = 800;
+// canvas.height= 650;
+
+// const keys= [];
+// const player = {
+//     x: 0, //hori posi 
+//     y: 0, //vert posi 
+//     width: 40, //size of player
+//     height: 40,  //size of player
+//     frameX: 0,  //hori coord - spreadsheet/sprite 
+//     frameY: 0, //vert coord - spreadsheet/sprite
+//     speed: 9,//pixel per frame
+//     moving: false //standing and walking animation
+// };
+
+
+// const shipSprite = new Image();
+// shipSprite.src = '../stylesheets/img/player_green.png';
+// // shipSprite.className = 'ship';
+// // shipSprite.style.height = 'auto';
+// // shipSprite.style.width = '40px';
+
+// const background = new Image();
+// background.src = '../stylesheets/img/background_blue.png';
+
+
+// // function drawSprite(img, sX, sY, sW, SH, dX, dY, dW, dH){
+// //     ctx.drawImage(img, sX, sY, sW, SH, dX, dY, dW, dH);
+// // } //!for big sprite with tilt effect
+// function drawSprite(img, dX, dY, dW, dH){
+//     ctx.drawImage(img, dX, dY, dW, dH);
+// }
+
+// let position = 0;
+// function animate(){
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.drawImage(background, 0, position, canvas.width, canvas.height);
+//     //built in canvas method. 
+//     position ++;
+//     // drawSprite(shipSprite, 0, 0, player.width, player.height,
+//     //     200, 150, player.width, player.hieght); //!for big sprite with tilt effect
+//     drawSprite(shipSprite, 200, 500, player.height, player.width);
+//     requestAnimationFrame(animate);
+// }
+
+// animate();
+
+// window.addEventListener('keydown', onKeyPress);
+// window.addEventListener('keyup', onKeyUp);
+
+// function onKeyPress(e){
+    
+// }
+
+// function onKeyUp(e){
+    
+// }
